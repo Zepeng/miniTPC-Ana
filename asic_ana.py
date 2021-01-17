5032db1dc1025e7762370450aedfbfb1a468fb74
Zepeng Li <zepengl@gmail.com>
2021-01-17 15:25:15 +0800

    update charge asic analysis.

--- asic_ana.py
+++ asic_ana.py
@@ -41,6 +41,7 @@ class ASIC_Ana:
         ax[0].plot(self.trigger['Time'][:3000], self.trigger['Ampl'][:3000], c='red')
         ax[1].plot(self.out['Time'][:3000], self.out['Ampl'][:3000], c='blue')
         plt.savefig(savename)
+        plt.close()
 
     def plot_channels(self, chs, savename):
         fig, ax = plt.subplots(len(chs), 2)
@@ -54,6 +55,30 @@ class ASIC_Ana:
             ax[i,1].set_xlabel('Time ($\mu s$)')
 
         plt.savefig(savename)
+        plt.close()
+
+    def plot_filtchannels(self, chs, savename):
+        fig, ax = plt.subplots(len(chs), 2)
+        for i in range(len(chs)):
+            sch = str(chs[i])
+            ax[i,0].plot(np.arange(len(self.filt_chan[sch])), self.filt_chan[sch])
+            ax[i,0].set_xlabel('Time ($\mu s$)')
+            ax[i,0].set_ylabel('Ch %d magnitude (V)' % chs[i])
+            lwf = len(self.filt_chan[sch])
+            ax[i,1].plot(np.arange(int(lwf/2)-150, int(lwf/2)+150), self.filt_chan[sch][int(lwf/2)-150:int(lwf/2)+150],color='red', label='Filtered' )
+            ax[i,1].plot(np.arange(int(lwf/2)-150, int(lwf/2)+150), self.amp_chan[sch][int(lwf/2)-150:int(lwf/2)+150], color='blue', label='Raw' )
+            ax[i,1].set_xlabel('Time ($\mu s$)')
+            ax[i,1].legend()
+
+        plt.savefig(savename)
+        plt.close()
+
+    def LowFilter(self, cutoff):
+        from scipy import signal
+        b, a = signal.butter(5, cutoff, fs=2000000, btype='low', analog=False)
+        self.filt_chan = {}
+        for key in self.amp_chan:
+            self.filt_chan[key] = signal.filtfilt(b, a, self.amp_chan[key])
 
     def CalcNoise(self, savename):
         fig1, ax1 = plt.subplots(6,6, figsize=(15,15))
@@ -70,11 +95,33 @@ class ASIC_Ana:
                 ax1[i, j].text(bins[1], 100, r'$\sigma$=%.4f V' % coeff[2])
         plt.tight_layout()
         fig1.savefig(savename)
+        plt.close()
+
+    def CalcFiltNoise(self, savename):
+        fig1, ax1 = plt.subplots(6,6, figsize=(15,15))
+        for i in range(6):
+            for j in range(6):
+                if j*6+i > 31:
+                    continue
+                lwf = len(self.filt_chan[str(i)])
+                n, bins, patches = ax1[i, j].hist(np.array(self.filt_chan[str(j*6+i)][:int(lwf/2)-100]), bins=20, histtype='step', color='blue')
+                bin_centers = (bins[:-1] + bins[1:])/2
+                p0=[200, np.mean(bins), 0.001]
+                coeff, var_matric = curve_fit(gauss, bin_centers, n, p0=p0)
+                ax1[i, j].plot(bin_centers, gauss(bin_centers, *coeff), color='red')
+                ax1[i, j].text(bins[1], 100, r'$\sigma$=%.4f V' % coeff[2])
+        plt.tight_layout()
+        fig1.savefig(savename)
+        plt.close()
 
 if __name__ == '__main__':
-    ftrigger = '/scratchfs/exo/zepengli94/pcbtile20210115/C3c4trigger_c3s1_c1out_c2s1_AC1M_50HzInCalOFFRealOFF33MHz_-5usdiv100mVdiv00001.csv'
-    fout = '/scratchfs/exo/zepengli94/pcbtile20210115/C1c4trigger_c3s1_c1out_c2s1_AC1M_50HzInCalOFFRealOFF33MHz_-5usdiv100mVdiv00005.csv'
-    ana = ASIC_Ana(ftrigger, fout)
-    ana.plot_tick('OneTick_N0.pdf')
-    ana.plot_channels([20, 21, 22], 'chans_N0.pdf')
-    ana.CalcNoise('Noise_N0.pdf')
+    for i in range(14):
+        ftrigger = '/workfs/exo/zepengli94/pcbtile20210116/C3c4trigger_c3s1_c1out_c2s1_AC1M_50HzInCalOFFRealOFF33MHz_-5usdiv100mVdiv000%02d.csv' % i
+        fout = '/workfs/exo/zepengli94/pcbtile20210116/C1c4trigger_c3s1_c1out_c2s1_AC1M_50HzInCalOFFRealOFF33MHz_-5usdiv100mVdiv000%02d.csv' % i
+        ana = ASIC_Ana(ftrigger, fout)
+        ana.LowFilter(200000)
+        ana.plot_tick('plots/OneTick_%d.pdf' % i)
+        ana.plot_channels([20, 21, 22], 'plots/chans_%d.pdf' % i)
+        ana.plot_filtchannels([20, 21, 22], 'plots/filtchans_%d.pdf' % i)
+        ana.CalcNoise('plots/Noise_%d.pdf' % i)
+        ana.CalcFiltNoise('plots/FiltNoise_%d.pdf' % i)
