44f65463c882408d937e9607679fd8a3ece865ab
Zepeng Li <zepengl@gmail.com>
2021-04-20 13:40:59 +0800

    minor update

--- asic_ana.py
+++ asic_ana.py
@@ -7,13 +7,14 @@ from scipy.optimize import curve_fit
 def gauss(x, *p):
     A, mu, sigma = p
     return A*np.exp(-(x-mu)**2/(2.0*sigma**2))
+
 class ASIC_Ana:
     def __init__(self, ftrigger, fout):
         self.dict_chan = {}
-        points = 1000/33.
+        points = 500/33.
         for i in range(33):
             self.dict_chan[str(i)] = []
-        for i in range(1000):
+        for i in range(500):
             chan = int(i/points)
             if i - chan*points  > 1 and i - chan*points < 33:
                 self.dict_chan[str(chan)].append(i)
@@ -25,7 +26,7 @@ class ASIC_Ana:
         for i in range(33):
             self.amp_chan[str(i)] = np.array([])
 
-        for i in range(len(out_amp) - 1000):
+        for i in range(len(out_amp) - 500):
             if trig[i] < 0.3 and trig[i+1] > 0.3:
                 for j in range(33):
                     chans = self.dict_chan[str(j)]
@@ -50,41 +51,61 @@ class ASIC_Ana:
 
     def plot_tick(self, savename):
         fig, ax = plt.subplots(2, 1)
-        ax[0].plot(self.trigger['Time'][:3000], self.trigger['Ampl'][:3000], c='red')
-        ax[1].plot(self.out['Time'][:3000], self.out['Ampl'][:3000], c='blue')
+        ax[0].plot(self.trigger['Time'][:1000], self.trigger['Ampl'][:1000], c='red')
+        ax[1].plot(self.out['Time'][:1000], self.out['Ampl'][:1000], c='blue')
         plt.savefig(savename)
         plt.close()
 
     def plot_channels(self, chs, savename, Filtered: bool =False):
         fig, ax = plt.subplots(len(chs), 2)
-        for i in range(len(chs)):
-            sch = str(chs[i])
+        if len(chs) == 1:
+            sch = str(chs[0])
             if Filtered:
-                ax[i,0].plot(np.arange(self.filt_chan[sch].shape[0]), self.filt_chan[sch][:,0])
+                ax[0].plot(np.arange(self.filt_chan[sch].shape[0]), self.filt_chan[sch][:,0])
             else:
-                ax[i,0].plot(np.arange(self.amp_chan[sch].shape[0]), self.amp_chan[sch][:,0])
-            ax[i,0].set_xlabel('Time ($\mu s$)')
-            ax[i,0].set_ylabel('Ch %d magnitude (V)' % chs[i])
+                ax[0].plot(np.arange(self.amp_chan[sch].shape[0]), self.amp_chan[sch][:,0])
+            ax[0].set_xlabel('Time ($\mu s$)')
+            ax[0].set_ylabel('Ch %d magnitude (V)' % chs[0])
             lwf = self.amp_chan[sch].shape[0]
             if Filtered:
-                ax[i,1].plot(np.arange(int(lwf/2)-150, int(lwf/2)+150), self.filt_chan[sch][int(lwf/2)-150:int(lwf/2)+150, 0],color='red', label='Filtered' )
-                ax[i,1].plot(np.arange(int(lwf/2)-150, int(lwf/2)+150), self.amp_chan[sch][int(lwf/2)-150:int(lwf/2)+150, 0], color='blue', label='Raw' )
+                ax[1].plot(np.arange(int(lwf/2)-150, int(lwf/2)+150), self.filt_chan[sch][int(lwf/2)-150:int(lwf/2)+150, 0],color='red', label='Filtered' )
+                ax[1].plot(np.arange(int(lwf/2)-150, int(lwf/2)+150), self.amp_chan[sch][int(lwf/2)-150:int(lwf/2)+150, 0], color='blue', label='Raw' )
             else:
-                ax[i,1].plot(np.arange(int(lwf/2)-150, int(lwf/2)+150), self.amp_chan[sch][int(lwf/2)-150:int(lwf/2)+150, 0] )
-            ax[i,1].set_xlabel('Time ($\mu s$)')
+                ax[1].plot(np.arange(int(lwf/2)-150, int(lwf/2)+150), self.amp_chan[sch][int(lwf/2)-150:int(lwf/2)+150, 0] )
+            ax[1].set_xlabel('Time ($\mu s$)')
+
+        else:
+            for i in range(len(chs)):
+                sch = str(chs[i])
+                if Filtered:
+                    ax[i,0].plot(np.arange(self.filt_chan[sch].shape[0]), self.filt_chan[sch][:,0])
+                else:
+                    ax[i,0].plot(np.arange(self.amp_chan[sch].shape[0]), self.amp_chan[sch][:,0])
+                ax[i,0].set_xlabel('Time ($\mu s$)')
+                ax[i,0].set_ylabel('Ch %d magnitude (V)' % chs[i])
+                lwf = self.amp_chan[sch].shape[0]
+                if Filtered:
+                    ax[i,1].plot(np.arange(int(lwf/2)-150, int(lwf/2)+150), self.filt_chan[sch][int(lwf/2)-150:int(lwf/2)+150, 0],color='red', label='Filtered' )
+                    ax[i,1].plot(np.arange(int(lwf/2)-150, int(lwf/2)+150), self.amp_chan[sch][int(lwf/2)-150:int(lwf/2)+150, 0], color='blue', label='Raw' )
+                else:
+                    ax[i,1].plot(np.arange(int(lwf/2)-150, int(lwf/2)+150), self.amp_chan[sch][int(lwf/2)-150:int(lwf/2)+150, 0] )
+                ax[i,1].set_xlabel('Time ($\mu s$)')
 
         plt.savefig(savename)
         plt.close()
 
-    def CalcNoise(self, savename):
+    def CalcNoise(self, savename, dp:int=5, Filtered:bool=False):
         fig1, ax1 = plt.subplots(6,6, figsize=(15,15))
         for i in range(6):
             for j in range(6):
                 if j*6+i > 31:
                     continue
                 lwf = self.amp_chan[str(i)].shape[0]
-                n, bins, patches = ax1[i, j].hist(np.array(self.amp_chan[str(j*6+i)][:int(lwf/2)-100, 0]), bins=20, histtype='step', color='blue')
+                n, bins, patches = ax1[i, j].hist(np.array(self.amp_chan[str(j*6+i)][:int(lwf/2)-100, dp]), bins=20, histtype='step', color='blue')
+                if Filtered:
+                    n, bins, patches = ax1[i, j].hist(np.array(self.filt_chan[str(j*6+i)][:int(lwf/2)-100, dp]), bins=20, histtype='step', color='blue')
                 bin_centers = (bins[:-1] + bins[1:])/2
+                print(bins, n)
                 p0=[200, np.mean(bins), 0.001]
                 coeff, var_matric = curve_fit(gauss, bin_centers, n, p0=p0)
                 ax1[i, j].plot(bin_centers, gauss(bin_centers, *coeff), color='red')
@@ -93,25 +114,9 @@ class ASIC_Ana:
         fig1.savefig(savename)
         plt.close()
 
-    def CalcFiltNoise(self, savename):
-        fig1, ax1 = plt.subplots(6,6, figsize=(15,15))
-        for i in range(6):
-            for j in range(6):
-                if j*6+i > 31:
-                    continue
-                lwf = self.filt_chan[str(i)].shape[0]
-                n, bins, patches = ax1[i, j].hist(np.array(self.filt_chan[str(j*6+i)][:int(lwf/2)-100, 0]), bins=20, histtype='step', color='blue')
-                bin_centers = (bins[:-1] + bins[1:])/2
-                p0=[200, np.mean(bins), 0.001]
-                coeff, var_matric = curve_fit(gauss, bin_centers, n, p0=p0)
-                ax1[i, j].plot(bin_centers, gauss(bin_centers, *coeff), color='red')
-                ax1[i, j].text(bins[1], 100, r'$\sigma$=%.4f V' % coeff[2])
-        plt.tight_layout()
-        fig1.savefig(savename)
-        plt.close()
     def NoiseFreq(self, ch):
         from scipy.fft import fft, fftfreq
-        T = 1/1000000.
+        T = 1/2000000.
         N = self.amp_chan[str(ch)].shape[0]
         batchsize = self.amp_chan[str(ch)].shape[1]
         batch_noise = []
@@ -132,14 +137,14 @@ class ASIC_Ana:
         plt.close()
 
 if __name__ == '__main__':
-    ftrigger = '/workfs2/exo/zepengli94/nexo/pcb_20210416/C3c4trigger_c3s1_c2sh31_c1out_DC50_1ms_CalON10fC_CLKON33MHz_10ch00001.csv'
-    fout = '/workfs2/exo/zepengli94/nexo/pcb_20210416/C1c4trigger_c3s1_c2sh31_c1out_DC50_1ms_CalON10fC_CLKON33MHz_10ch00001.csv'
+    ftrigger = '/scratchfs/exo/zepengli94/nexo/pcb_20210418/C3c4trigger_c3s1_c2sh31_c1out_DC50_1ms_CalON10fC_CLKON66MHz_10ch00000.csv'
+    fout = '/scratchfs/exo/zepengli94/nexo/pcb_20210418/C1c4trigger_c3s1_c2sh31_c1out_DC50_1ms_CalON10fC_CLKON66MHz_10ch00000.csv'
     ana = ASIC_Ana(ftrigger, fout)
+    ana.plot_tick('plots/OneTick.pdf' )
+    for n in range(5, 12):
+        ana.CalcNoise('plots/Noise_dp_%d.pdf' %  n, n)
     ana.LowFilter(200000)
     for i in range(14):
-        ana.plot_tick('plots/OneTick_%d.pdf' % i)
-        ana.plot_channels([i, 21], 'plots/chans_%d.pdf' % i)
-        ana.plot_channels([i, 21], 'plots/filt_chans_%d.pdf' % i, True)
-        ana.CalcNoise('plots/Noise_%d.pdf' % i)
-        ana.CalcFiltNoise('plots/FiltNoise_%d.pdf' % i)
+        ana.plot_channels([i], 'plots/chans_%d.pdf' % i)
+        ana.plot_channels([i], 'plots/filt_chans_%d.pdf' % i, True)
         ana.NoiseFreq(i)
